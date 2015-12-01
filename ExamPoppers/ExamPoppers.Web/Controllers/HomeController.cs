a07f2bfd8621c6e9c3f1df6a9c5b4969414a3f7e
eawright03 <e.a.wright03@gmail.com>
2015-12-01 11:32:09 -0500

    AddQuestion button returns question to controller. Now just need to add it to db and refresh screen!

--- ExamPoppers/ExamPoppers.Web/Controllers/HomeController.cs
+++ ExamPoppers/ExamPoppers.Web/Controllers/HomeController.cs
@@ -21,7 +21,7 @@ namespace ExamPoppers.Web.Controllers
             return View();
         }
         [HttpGet]
-        public ActionResult AddQuestion()
+        public ActionResult AddQuestionAndQuestionList()
         {
             var query =
                 from q in db.Question
@@ -39,11 +39,40 @@ namespace ExamPoppers.Web.Controllers
             }
             return View(qList);
         }
-        //[HttpPost]
-        //public ActionResult AddQuestion()
-        //{
-        //    return View();
-        //}
+        [HttpPost]
+        public ActionResult AddQuestionAndQuestionList(Question que)
+        {
+            var query =
+                from q in db.Question
+                select new { Content = q.Content, Answer = q.Answer };
+            query.ToList();
+
+            List<Question> qList = new List<Question>();
+            foreach (var x in query)
+            {
+                qList.Add(new Question
+                {
+                    Content = x.Content,
+                    Answer = x.Answer
+                });
+            }
+            return View(qList);
+        }
+        [HttpPost]
+        public ActionResult AddQuestion(Question q)
+        {
+            //add q to db
+            var query =
+                from question in db.Question
+                select new { Content = question.Content };
+            query.ToList();
+           // q = new Question();
+            q.Id = query.Count();
+            db.Question.Insert(q);
+         //NEED TP RE-RENDER SCREEN
+
+            return View("AddQuestionAndQuestionList");
+        }
         public ActionResult NavBar()
         {
             return PartialView("_NavBar");
